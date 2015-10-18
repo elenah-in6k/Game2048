c1fc380c21e03b726da25099aef4d1cdd21646cd
ElenaH <elenah@interlink-ua.com>
2015-10-19 02:48:06 +0300

    printer

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -10,15 +10,16 @@ public class Printer {
     void printGameField(){
         int i=0;
         while (i < gameField.SIZE * (gameField.SIZE - 1) ) {
-                System.out.println("--------------------------------");
+                System.out.println(" -----   -----   -----   -----  ");
             int j = 0;
-            while (j*i < i* gameField.SIZE){
+            while (((i + 1) * j - 1) < ((i+1)*gameField.SIZE - 1)){
             System.out.print("|");
-            System.out.printf("%4d", gameField.gameField.get(i).cellValue);
-            i++;
-            System.out.print("|");
+            System.out.printf("%4d", gameField.gameField.get(j).cellValue);
+                System.out.print(" | ");
                 j++;
-            } System.out.println("--------------------------------");
+            }
+            System.out.println();
+            System.out.println(" -----   -----   -----   -----  ");
 
             i += gameField.SIZE;
         }
